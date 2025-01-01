356e5ed1b363f1375e6b8c3e8f76ca99c34785b1
SAKTHIVAISHNAVI N <sakthivaishnavi12@gmail.com>
2025-01-01 20:30:02 +0530

    Remove unused dependencies and clean up package files

--- myportfolio/src/App.jsx
+++ myportfolio/src/App.jsx
@@ -1,32 +1,38 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import About from "./Components/About/About";
-import Hero from "./Components/Hero/Hero"
+import Hero from "./Components/Hero/Hero";
 import Education from "./Components/Education/Education";
 import Skills from "./Components/Skills/Skills";
 import Projects from "./Components/Projects/Projects";
 import Achievements from "./Components/Achievements/Achievements";
 import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
+import { LightSpeed } from "react-swift-reveal";
+import { useInView } from 'react-intersection-observer';
 
 function App() {
+  const { ref, inView } = useInView({ triggerOnce: false, threshold: 0.1 });
+
   return (
     <>
-    <div>
-    <div className='container mx-auto text-white'>
-          <Navbar />
-          <Hero />
-          <About />
-          <Education />
-          <Skills />
-          <Projects />
-          <Achievements />
-          <Contact/>
-          <Footer />
+      <div className='container mx-auto text-white'>
+        <Navbar />
+        <Hero />
+        <div ref={ref}>
+          <LightSpeed key={inView ? 'inView' : 'notInView'} left={true} duration={2000}>
+            <About />
+          </LightSpeed>
         </div>
-    </div>
+        <Education />
+        <Skills />
+        <Projects />
+        <Achievements />
+        <Contact />
+        <Footer />
+      </div>
     </>
-   
-  )
+  );
 }
+
 export default App;
