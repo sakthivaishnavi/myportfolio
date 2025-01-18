f75482864d8356158fd56e1a51a3caa5bca1eee7
SAKTHIVAISHNAVI N <sakthivaishnavi12@gmail.com>
2025-01-19 00:18:49 +0530

    updated achievements

--- myportfolio/src/Components/Achievements/Achievements.jsx
+++ myportfolio/src/Components/Achievements/Achievements.jsx
@@ -10,32 +10,26 @@ const Achievements = () => {
         <hr className='border-[#353d3c] w-3/4'/>
       </div>
       <div ref={ref}>
-        <Fade key={inView ? 'inView' : 'notInView'} top={true} duration={1000}>
+        <Fade key={inView ? 'inView' : 'notInView'} top={true} duration={1500}>
       <h1 className='text-4xl text-center'>Accomplishments</h1>
       </Fade>
       </div>
-      <div ref={ref}>
+      <div ref={ref} className='flex justify-center'>
         <Fade key={inView ? 'inView' : 'notInView'} right={true} duration={2000}>
-        <table className='table-auto mx-auto mt-20'>
-          <tbody>
-            <tr className='bg-gray-950 text-[#5eead4]'>
-              <td className='px-4 py-2'>30/09/2023</td>
-              <td className='px-4 py-2'>1st prize in Brain Baffle, KEC</td>
-            </tr>
-            <tr className='bg-gray-900 text-[#5eead4]'>
-              <td className='px-4 py-2'>2020</td>
-              <td className='px-4 py-2'>1st prize in Mathcon, KEC</td>
-            </tr>
-            {/* <tr className='bg-gray-950 text-[#5eead4]'>
-              <td className='border border-[#5eead4] border-dotted px-4 py-2'>30/09/2023</td>
-              <td className='border border-[#5eead4] border-dotted px-4 py-2'>3rd prize in Guess the Code, KEC</td>
-            </tr> */}
-            <tr className='bg-gray-950 text-[#5eead4]'>
-              <td className='px-4 py-2'>30/09/2023</td>
-              <td className='px-4 py-2'>3rd prize in Mystery Box, KEC</td>
-            </tr>
-          </tbody>
-        </table>
+        <div className="relative border-l-2 border-teal-500 mt-20 mx-10">
+  {[
+    { date: "30/09/2023", title: "1st prize in Brain Baffle, KEC" },
+    { date: "30/09/2023", title: "3rd prize in Mystery Box, KEC" },
+    { date: "19/03/2024", title: "1st prize in Mathcon, KEC" },
+  ].map((achievement, index) => (
+    <div key={index} className="mb-10 ml-6">
+      <div className="absolute -left-4 w-8 h-8 bg-teal-500 rounded-full"></div>
+      <p className="text-gray-400">{achievement.date}</p>
+      <h3 className="text-[#5eead4] font-semibold">{achievement.title}</h3>
+    </div>
+  ))}
+</div>
+
         </Fade>
       </div>
       <div className='flex justify-center mt-24 mb-20'>
